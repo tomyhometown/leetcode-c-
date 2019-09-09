6bc71ccb16f9bb137ebdad4d02370822ae3c1ead
tomyhometown <dingzi1129@gmail.com>
2019-09-09 18:15:09 +0800

    update 2

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -12,55 +12,84 @@
  * };
  */
 #include<math.h>
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(nullptr) {}
+// };
+
 class Solution {
 public:
-    // struct ListNode {
-    //     int val;
-    //     ListNode *next;
-    //     ListNode(int x) : val(x), next(NULL) {}
-    // };
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *l3;
-        int digit;
-        if(l1 != nullptr && l2 != nullptr && l3 != nullptr)
+        ListNode *head = new ListNode(-1);
+        int digit = 0;
+        head->val = (digit + l1->val + l2->val) % 10;
+        digit = (digit + l1->val + l2->val) / 10;
+        head->next = nullptr;
+        ListNode *end = new ListNode(-1);
+        end = head;
+        l1 = l1->next;
+        l2 = l2->next;
+        while (l1 != nullptr || l2 != nullptr)
         {
-            l3->val = (l1->val + l2->val) % 10;
-            digit = (l1->val + l2->val) / 10;
-        }
-        while (l1->next != nullptr || l2->next != nullptr)
-        {
-            l1 = l1->next;
-            l2 = l2->next;
             if(l1 != nullptr && l2 != nullptr)
             {
-                ListNode *l;
-                if(l1 != nullptr && l2 != nullptr && l3 != nullptr)
-                {
-                    l->val = digit + (l1->val + l2->val) % 10;
-                    digit = (l1->val + l2->val) / 10;
-                    l3->next = l;
-                }
+                ListNode *node = new ListNode(-1);
+                node->val = (digit + l1->val + l2->val) % 10;
+                digit = (digit + l1->val + l2->val) / 10;
+                end->next = node;
+                end = node;
+                l1 = l1->next;
+                l2 = l2->next;
             }
-            if(l1 == nullptr)
+            if(l1 == nullptr && l2 != nullptr)
             {
-                l3->next->val = digit + l2->val;
-                l3->next->next = l2->next;
-                break;
+                ListNode *node = new ListNode(-1);
+                node->val = (digit + l2->val) % 10;
+                digit = (digit + l2->val) / 10;
+                end->next = node;
+                end = node;
+                l2 = l2->next;
             }
-            if(l2 == nullptr)
+            if(l2 == nullptr && l1 != nullptr)
             {
-                if(l1 != nullptr && l3 !=nullptr && l3->next != nullptr)
-                {
-                    l3->next->val = digit + l1->val;
-                }
-                if(l1->next != nullptr && l3->next->next != nullptr)
-                {
-                    l3->next->next = l1->next;
-                }
-                break;
+                ListNode *node = new ListNode(-1);
+                node->val = (digit + l1->val) % 10;
+                digit = (digit + l1->val) / 10;
+                end->next = node;
+                end = node;
+                l1 = l1->next;
             }
         }
-        return l3;
+        if(digit != 0)
+        {
+            ListNode *node = new ListNode(-1);
+            node->val = digit;
+            end->next = node;
+            end = node;
+        }
+        end->next = nullptr;
+        return head;
     }
 };
+
+// int main()
+// {
+//     Solution solution;
+//     ListNode *l1, *l2;
+//     l1 = new ListNode(-1);
+//     l2 = new ListNode(-1);
+//     l1->next = new ListNode(-1);
+//     l2->next = new ListNode(-1);
+//     l1->next->next = new ListNode(-1);
+//     l2->next->next = new ListNode(-1);
+//     l1->val = 2;
+//     l1->next->val = 4;
+//     l1->next->next->val = 3;
+//     l2->val = 5;
+//     l2->next->val = 6;
+//     l2->next->next->val = 4;
+//     solution.addTwoNumbers(l1, l2);
+// }
 
